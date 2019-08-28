16e8b9dd647fe5605aa2efd3b4ab2ed111fc08ce
Jatin Mayekar <44420409+gonfreces@users.noreply.github.com>
2019-08-28 14:06:56 -0600

    New sensor logic update
    Add logic to consider two new sensors - reflective sensors added on each side of the robot.
    Purpose of the sensor is to detect the yellow line.
    Logic - Detect the yellow line - change the speed of the wheels to keep the robot between the yellow lines

--- LIne_Follower_Arduino.ino
+++ LIne_Follower_Arduino.ino
@@ -1,112 +1,128 @@
-int sensorPin = A5;    // select the input pin for the potentiometer
-
 int k =0;
 
-
 const int AIN1 = 13;           //control pin 1 on the motor driver for the right motor
-const int AIN2 = 12;            //control pin 2 on the motor driver for the right motor
-const int PWMA = 11;            //speed control pin on the motor driver for the right motor
+const int AIN2 = 12;           //control pin 2 on the motor driver for the right motor
+const int PWMA = 11;           //speed control pin on the motor driver for the right motor
 
-const int BIN1 = 9;           //control pin 1 on the motor driver for the right motor
-const int BIN2 = 8;            //control pin 2 on the motor driver for the right motor
-const int PWMB = 10;            //speed control pin on the motor driver for the right motor
 
-const int BIN1_2 = 7;           //control pin 1 on the motor driver for the right motor
-const int BIN2_2 = 6;            //control pin 2 on the motor driver for the right motor
-const int PWMC = 5;            //speed control pin on the motor driver for the right motor
+const int AIN3 = 8;           //control pin 1 on the motor driver for the right motor
+const int AIN4 = 7;           //control pin 2 on the motor driver for the right motor
+const int PWMB = 6;           //speed control pin on the motor driver for the right motor
 
-const int AIN1_2 = 4;           //control pin 1 on the motor driver for the right motor
-const int AIN2_2 = 2;            //control pin 2 on the motor driver for the right motor
-const int PWMD = 3 ;    
-int sensorValue = 0;  
-int motorSpeed1 = 155;
-int motorSpeed2 = 80;
+const int AIN5 = 9;           //control pin 1 on the motor driver for the right motor
+const int AIN6 = 10;           //control pin 2 on the motor driver for the right motor
+const int PWMC = 5;           //speed control pin on the motor driver for the right motor
+
+const int AIN7 = 4;           //control pin 1 on the motor driver for the right motor
+const int AIN8 = 2;           //control pin 2 on the motor driver for the right motor
+const int PWMD = 3;           //speed control pin on the motor driver for the right motor
+
+const int motorSpeed1 = 125;
+const int motorSpeed2 = 80;
+
+int sv1 = 0;
+int sv2 = 0;
+
+int s1 = A5;
+int s2 = A4;
 
 void setup() 
 {
-    pinMode(AIN1, OUTPUT);
-    pinMode(AIN2, OUTPUT);
-    pinMode(PWMA, OUTPUT);
-    pinMode(BIN1, OUTPUT);
-    pinMode(BIN2, OUTPUT);
-    pinMode(PWMB, OUTPUT);
-
-    pinMode(AIN1_2, OUTPUT);
-    pinMode(AIN2_2, OUTPUT);
-    pinMode(PWMD, OUTPUT);
-
-    pinMode(BIN1_2, OUTPUT);
-    pinMode(BIN2_2, OUTPUT);
-    pinMode(PWMC, OUTPUT);
-
-
-   Serial.begin(9600);
-  //pinMode(ledPin, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+    
+  pinMode(AIN3, OUTPUT);
+  pinMode(AIN4, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  
+  pinMode(AIN5, OUTPUT);
+  pinMode(AIN6, OUTPUT);
+  pinMode(PWMC, OUTPUT);
+  
+  pinMode(AIN7, OUTPUT);
+  pinMode(AIN8, OUTPUT);
+  pinMode(PWMD, OUTPUT);
+  
+  Serial.begin(9600);
 }
+
 
 void loop() 
 {
   
-  sensorValue = analogRead(sensorPin);
-  
-  Serial.print("\n");
-  Serial.print(k);
-  
-  Serial.print("\n");
-  Serial.print(sensorValue);
-  
-  if (sensorValue<600)
+  sv1 = analogRead(s1);
+  sv2 = analogRead(s2);
+  if ((sv1<600) && (sv2>600))
   {
-    sensorValue = analogRead(sensorPin);
-    k=1;                                    //if yellow line detected 
+    k=1;
+    sv1 = analogRead(s1);
+    sv2 = analogRead(s2);
     
-    digitalWrite(13, HIGH);                 //clockwise Front left              
+    digitalWrite(13, HIGH);           //clockwise Front left              
     digitalWrite(12, LOW);
-    analogWrite(PWMA, abs(motorSpeed1));
+    analogWrite(PWMA, abs(motorSpeed1));       
+
+    digitalWrite(8, HIGH);           //clockwise Front left              
+    digitalWrite(7, LOW);
+    analogWrite(PWMB, abs(motorSpeed2+50));
     
-    digitalWrite(9, LOW);                         
-    digitalWrite(8, HIGH);                  //clockwise rear left
-    analogWrite(PWMB, abs(motorSpeed1));
-    
-    digitalWrite(7, HIGH);                  //clockwise rear right 
-    digitalWrite(6, LOW);
-    analogWrite(PWMC, abs(motorSpeed2));  
-    
-    digitalWrite(4, HIGH);                  //clockwise front right 
-    digitalWrite(2, LOW);
-    analogWrite(PWMD, abs(motorSpeed2)); 
-    
-    //Serial.print("\n");
-    //Serial.print(sensorValue);
-    
+    Serial.print("\n");
+    Serial.print(k);
   }
-  else
+
+  if ((sv1>600) && (sv2<600))
   {
+    k=2;
+    sv1 = analogRead(s1);
+    sv2 = analogRead(s2);
     
-    sensorValue = analogRead(sensorPin);
-    k=0;
-    digitalWrite(13, HIGH);             //clockwise Front left              
+    digitalWrite(13, HIGH);           //clockwise Front left              
     digitalWrite(12, LOW);
-    analogWrite(PWMA, abs(motorSpeed2));    
-    digitalWrite(9, LOW);                         
-    digitalWrite(8, HIGH);              //clockwise rear left
+    analogWrite(PWMA, abs(motorSpeed1+50));       
+
+    digitalWrite(8, HIGH);           //clockwise Front left              
+    digitalWrite(7, LOW);
     analogWrite(PWMB, abs(motorSpeed2));
-    digitalWrite(7, HIGH);              //clockwise rear right 
-    digitalWrite(6, LOW);
-    analogWrite(PWMC, abs(motorSpeed1));  
-    digitalWrite(4, HIGH);              //clockwise front right 
-    digitalWrite(2, LOW);
-    analogWrite(PWMD, abs(motorSpeed1)); 
+    
+    Serial.print("\n");
+    Serial.print(k);
   }
   
-  Serial.print("\n");
-  Serial.print(k);
-  
-  
-  //Serial.print("sensor = \n");
-  //Serial.print(sensorValue);
-  delay(100);
+  if ((sv1<600) && (sv2<600))
+    {
+    k=3;
+    sv1 = analogRead(s1);
+    sv2 = analogRead(s2);
+    
+    digitalWrite(13, HIGH);           //clockwise Front left              
+    digitalWrite(12, LOW);
+    analogWrite(PWMA, abs(motorSpeed1));       
 
+    digitalWrite(8, HIGH);           //clockwise Front left              
+    digitalWrite(7, LOW);
+    analogWrite(PWMB, abs(motorSpeed2));
+    
+    Serial.print("\n");
+    Serial.print(k);
+    }
+  if ((sv1>600) && (sv2>600))
+    {
+     k=4;
+     sv1 = analogRead(s1);
+     sv2 = analogRead(s2);
+    
+     digitalWrite(13, LOW);           //clockwise Front left              
+     digitalWrite(12, HIGH);
+     analogWrite(PWMA, abs(motorSpeed1));       
+
+     digitalWrite(8, LOW);           //clockwise Front left              
+     digitalWrite(7, HIGH);
+     analogWrite(PWMB, abs(motorSpeed2));
+    
+     Serial.print("\n");
+     Serial.print(k);
+
+    }
+  delay(1000);
 }
-
-
